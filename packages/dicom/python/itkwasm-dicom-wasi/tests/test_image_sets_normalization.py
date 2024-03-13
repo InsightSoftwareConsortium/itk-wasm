4293f034f913fad39b26026542a8b286608b3493
Paul Elliott <paul.elliott@kitware.com>
2024-03-13 15:26:26 -0400

    Fix generated file import of image

--- packages/dicom/python/itkwasm-dicom-wasi/tests/test_image_sets_normalization.py
+++ packages/dicom/python/itkwasm-dicom-wasi/tests/test_image_sets_normalization.py
@@ -1,7 +1,5 @@
 # Generated file. To retain edits, remove this comment.
 
-from itkwasm_dicom_wasi import image
-
 from .common import test_input_path, test_output_path
 
 def test_sort_dicom_series():
@@ -13,4 +11,3 @@ def test_sort_dicom_series():
 
     output_text = image_sets_normalization([test_file_path])
     assert output_text
-    
